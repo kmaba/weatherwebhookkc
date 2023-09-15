0dab2f3a1717f8708911bbc7589c7a7331ad9b41
Abdullah Arafat <62858251+kmaba@users.noreply.github.com>
2023-09-15 16:20:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ def get_weather_data():
     latitude = -31.9781
     longitude = 115.9556
 
-    # OpenWeatherMap API endpoint for current weather data
-    weather_api_url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}"
+    # OpenWeatherMap One Call API endpoint for forecast data
+    weather_api_url = f"https://api.openweathermap.org/data/2.5/onecall?lat={latitude}&lon={longitude}&exclude=current,minutely,hourly,alerts&appid={api_key}"
 
     response = requests.get(weather_api_url)
     data = response.json()
@@ -20,26 +20,30 @@ def get_weather_data():
 
 def post_to_discord(data, next_update_time=None):
     # Replace this with your new Discord webhook URL
-    webhook_url = "https://discord.com/api/webhooks/1152081939692531792/ge9OfoVVONFOVL_n1HOjMsS_wAThanFyS8d_9wEfv5CD9qZnOnhiQKYU1ED5Dwf93bdx"
+    webhook_url = "https://discord.com/api/webhooks/1152075250931073135/-AOJcH8y5KHn5A6yXvk5vmG0eGeL4_DGr2PLrtESLBXnllIvVkM4_WfoXCjbvcHbngdW"
 
     # Extract temperature information from the API response
-    if "main" in data and "temp" in data["main"]:
-        temperature_kelvin = data["main"]["temp"]
-        temperature_celsius = temperature_kelvin - 273.15  # Convert from Kelvin to Celsius
-        temperature_formatted = f"{temperature_celsius:.2f}°C"
+    if "daily" in data and len(data["daily"]) > 0:
+        temperature_high_kelvin = data["daily"][0]["temp"]["max"]
+        temperature_low_kelvin = data["daily"][0]["temp"]["min"]
+        temperature_high_celsius = temperature_high_kelvin - 273.15  # Convert from Kelvin to Celsius
+        temperature_low_celsius = temperature_low_kelvin - 273.15  # Convert from Kelvin to Celsius
+        temperature_high_formatted = f"{temperature_high_celsius:.2f}°C"
+        temperature_low_formatted = f"{temperature_low_celsius:.2f}°C"
     else:
-        temperature_formatted = "N/A"
+        temperature_high_formatted = "N/A"
+        temperature_low_formatted = "N/A"
 
     # Extract weather condition, wind speed, and other relevant data
-    if "weather" in data and len(data["weather"]) > 0 and "description" in data["weather"][0]:
-        condition = data["weather"][0]["description"]
+    if "weather" in data["daily"][0] and len(data["daily"][0]["weather"]) > 0 and "description" in data["daily"][0]["weather"][0]:
+        condition = data["daily"][0]["weather"][0]["description"]
     else:
         condition = "N/A"
 
-    if "wind" in data:
-        wind_speed = data["wind"]["speed"]
+    if "wind_speed" in data["daily"][0] and "wind_deg" in data["daily"][0]:
+        wind_speed = data["daily"][0]["wind_speed"]
         wind_speed_formatted = f"{wind_speed}km/h"
-        wind_direction_degrees = data["wind"]["deg"]
+        wind_direction_degrees = data["daily"][0]["wind_deg"]
         wind_direction_cardinal = ["N", "NE", "E", "SE", "S", "SW", "W", "NW", "N"][round(wind_direction_degrees / 45) % 8]
     else:
         wind_speed_formatted = "N/A"
@@ -53,12 +57,13 @@ def post_to_discord(data, next_update_time=None):
     # Format the message with the corrected temperature, condition, wind speed, and additional line
     message_parts = [
         f"{current_date_awst_str}:",
-        f"> Expected {condition} with a top of {temperature_formatted}",
-        f"> Wind speeds of around {wind_speed_formatted} expected from the {wind_direction_cardinal}, insha'allah."
+        f"Expected {condition} with a high of {temperature_high_formatted} and a low of {temperature_low_formatted}",
+        f"Wind speeds of around {wind_speed_formatted} expected from the {wind_direction_cardinal}, insha'allah...",
+        "<https://openweathermap.org/city/2063523>"
     ]
 
     if next_update_time is not None:
-        message_parts.append(f"> Next weather message is <t:{next_update_time}:R>")
+        message_parts.append(f"Next update: <t:{next_update_time}:R>")
 
     message_parts.append("ᵐᵃᵈᵉ ᵇʸ ᵃᵇᵈᵘˡˡᵃʰ ᵃʳᵃᶠᵃᵗ")
     
